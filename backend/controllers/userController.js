8fd8f6ceed2e769e84e23c65c5116b68124af598
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-05-04 22:34:03 +0530

    add bcryptjs file

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken'
 
 const createToken = (id)=>{
