6504588a95bb4fe68fa8a34351a74874fb6ee335
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-05-04 22:49:21 +0530

    change name to OurPolicy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/ourPolicy'
+import OurPolicy from '../components/OurPolicy'
 const Home = () => {
   return (
     <div className='w-full'>
